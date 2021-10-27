e085f2df42a99e6645c4b35b5752feccb66265d1
SierraBowfin <sreten.vaso@gmail.com>
2021-10-27 23:36:09 +0200

    depricated data

--- upload-dataset-to-hdfs.sh
+++ upload-dataset-to-hdfs.sh
@@ -11,17 +11,3 @@ then
   echo "[INFO]: Adding data.csv in the /big-data folder on the HDFS"
   docker exec -it namenode hdfs dfs -put /big-data/data.csv /big-data/data.csv
 fi
-
-# docker exec -it namenode hdfs dfs -test -e /big-data/events.csv
-# if [ $? -eq 1 ]
-# then
-#   echo "[INFO]: Adding events.csv in the /big-data folder on the HDFS"
-#   docker exec -it namenode hdfs dfs -put /big-data/events.csv /big-data/events.csv
-# fi
-
-# docker exec -it namenode hdfs dfs -test -e /big-data/filt-data.csv
-# if [ $? -eq 1 ]
-# then
-#   echo "[INFO]: Adding filt-data.csv in the /big-data folder on the HDFS"
-#   docker exec -it namenode hdfs dfs -put /big-data/filt-data.csv /big-data/filt-data.csv
-# fi
